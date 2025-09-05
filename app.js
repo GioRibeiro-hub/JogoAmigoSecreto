636859c12a98f47d76d3953c2c0c67223440c275
GioRibeiro <giovanniribeirods3@gmail.com>
2025-09-05 11:41:43 -0300

    Adicionando a função adicionarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+    let campo = document.getElementById("amigo");
+    let nome = campo.value.trim();
+
+    if (nome === ""){
+
+        alert("Por favor, insira um nome.")
+        return;
+    }
+
+    amigos.push(nome);
+
+    atualizarLista();
+
+    campo.value = "";
+}
