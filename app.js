7971bad65548ee0667a22ddaec21c05da1a6d684
GioRibeiro <giovanniribeirods3@gmail.com>
2025-09-06 00:46:31 -0300

    funçao atualizar lista

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let amigos = [];
 
 function adicionarAmigo() {
@@ -15,4 +16,15 @@ function adicionarAmigo() {
     atualizarLista();
 
     campo.value = "";
+}
+
+function atualizarLista(){
+    let lista = document.getElementById("listaAmigos")
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+    let item = document.createElement("li"); 
+    item.textContent = amigos[i];           
+    lista.appendChild(item); 
+    }
 }
